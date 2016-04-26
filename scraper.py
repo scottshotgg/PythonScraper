4b2f3ebc283176329e8fe9b4753d99919761b023
scottshotgg <scgaydos@gmail.com>
2016-04-26 11:41:34 -0500

    split all the data up by spaces

--- scraper.py
+++ scraper.py
@@ -10,14 +10,47 @@ pTag = tree.xpath('//div[@class="over"]/p')
 
 p_content = [p.text_content() for p in pTag]
 
+totalInformation = [title[0]]
+
+'''
 print '\n' + title[0]
 
 print '\n\n'
+'''
+# add that in to see if we can detect that
 
 for x in range(len(extraInformation)):
-	print extraInformation[x] + '\n'
+	totalInformation.append(extraInformation[x])
+	#print extraInformation[x] + '\n'
 
 print '\n'
 
 for x in range(len(p_content)):
-	print p_content[x] + '\n' 
+	totalInformation.append(p_content[x])
+#print p_content[x] + '\n' 
+
+#print totalInformation
+
+
+
+#print totalInformation
+
+#layer = title[0].split(' ')
+totalInformationSplit = []
+z = 0
+
+for x in range(len(totalInformation)):
+	#print totalInformation[x]
+	split = totalInformation[x].split(' ')
+	for y in range(len(split)):
+		totalInformationSplit.append(split[y])
+		'''
+		print '\n'
+		print y
+		print z
+		z += 1
+		print '\n'
+		'''
+
+for x in range(len(totalInformationSplit)):
+	print totalInformationSplit[x]
