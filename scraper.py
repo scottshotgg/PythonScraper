7526cd53cc61881ed32a95b08b7855a40a1d12ca
scottshotgg <kind3rgartenlunchbox@gmail.com>
2016-05-01 13:21:03 -0500

    added sentences found, still working on NER,

--- scraper.py
+++ scraper.py
@@ -15,22 +15,22 @@ terminals = ['!', '?', u'…', u'.']
 
 # This function strips out only the alphacharacters and the period
 def stripAlphaChars(word):
-	russianAlphaNumericChars =[
-							u'а', u'б', u'в', u'г', u'д', u'е', u'ё', u'ж', u'з', u'и', u'й', 
-							u'к', u'л', u'м', u'н', u'о', u'п', u'р', u'с', u'т', u'у', u'ф', 
-							u'х', u'ц', u'ч', u'ш', u'щ', u'ъ', u'ы', u'ь', u'э', u'ю', u'я', 
-							u'А', u'Б', u'В', u'Г', u'Д', u'Е', u'Ё', u'Ж', u'З', u'И', u'Й', 
-							u'К', u'Л', u'М', u'Н', u'О', u'П', u'Р', u'С', u'Т', u'У', u'Ф', 
-							u'Х', u'Ц', u'Ч', u'Ш', u'Щ', u'Ъ', u'Ы', u'Ь', u'Э', u'Ю', u'Я',
-							u'0', u'1', u'2', u'3', u'4', u'5', u'6', u'7', u'8', u'9'
-						]
+	russianAlphaNumericChars = [
+								u'а', u'б', u'в', u'г', u'д', u'е', u'ё', u'ж', u'з', u'и', u'й', 
+								u'к', u'л', u'м', u'н', u'о', u'п', u'р', u'с', u'т', u'у', u'ф', 
+								u'х', u'ц', u'ч', u'ш', u'щ', u'ъ', u'ы', u'ь', u'э', u'ю', u'я', 
+								u'А', u'Б', u'В', u'Г', u'Д', u'Е', u'Ё', u'Ж', u'З', u'И', u'Й', 
+								u'К', u'Л', u'М', u'Н', u'О', u'П', u'Р', u'С', u'Т', u'У', u'Ф', 
+								u'Х', u'Ц', u'Ч', u'Ш', u'Щ', u'Ъ', u'Ы', u'Ь', u'Э', u'Ю', u'Я',
+								u'0', u'1', u'2', u'3', u'4', u'5', u'6', u'7', u'8', u'9'
+							   ]
 	x = 0
 	while x < len(word):
 		if word[x] not in russianAlphaNumericChars and word[x] not in terminals:
 			word = word.replace(word[x], u'')
 			x -= 1
 		x += 1
-	print "\"" + word + "\""
+	#print "\"" + word + "\""
 	return word
 
 # Initialize the dictionary
@@ -41,8 +41,8 @@ print d.check(u'ученья..')
 print u'Калигула' in customWords
 
 # Input news.tj article
-page = requests.get('http://news.tj/ru/news/teatr-mayakovskogo-za-chto-smertnaya-kazn')
-#page = requests.get('http://news.tj/ru/node/225178')
+#page = requests.get('http://news.tj/ru/news/teatr-mayakovskogo-za-chto-smertnaya-kazn')
+page = requests.get('http://news.tj/ru/node/225178')
 
 # De-stringify it into a tree form
 tree = html.fromstring(page.content)
@@ -89,9 +89,6 @@ print "Time Accessed  : " + preliminaryInformation['a_time'] + "\n"
 # Honorifics array to hold abbreviations and abbreviations that we will need to analyze sentence terminals
 abbreviations = [u'гг.']
 
-
-
-
 # This will hold all of our sentences
 sentenceArray = []
 
@@ -113,11 +110,11 @@ for x in range(len(totalInformationSplit)):
 	# Replace the '!', and '?' that may have been before a '.'
 	word = word.replace('!', '').replace('?', '')
 	# Check if that last char is a period
-	if(lastChar == u'.'):
+	if lastChar == u'.':
 		# If its not a word the assume that it is not the end of a sentence
-		if(not(d.check(word))):
+		if not d.check(word):
 			# If it does happen to be a word in the custom words then it is a word
-			if((word.replace('.', u'').replace(u'…', u'') in customWords)):
+			if word.replace('.', u'').replace(u'…', u'') in customWords:
 				sentenceArray.append(sentence + totalInformationSplit[x])
 				sentence = ""
 			# If not just treat it as if it was not a word
@@ -127,21 +124,23 @@ for x in range(len(totalInformationSplit)):
 		# that they are single letter representing the first initial of someones name
 		else:
 			# If it is just two characters, i.e, a letter and a period
-			if(len(totalInformationSplit[x]) < 3):
+			if len(totalInformationSplit[x]) < 3:
 				sentence += totalInformationSplit[x] + " "
 			else:
 			# Else assume that the terminal is the ending of a sentence
 				sentenceArray.append(sentence + totalInformationSplit[x])
 				sentence = ""
 	# If it is one of the terminals then it is automatically the ending of a sentence
-	elif(lastChar in terminals):
+	elif lastChar in terminals:
 		sentenceArray.append(sentence + totalInformationSplit[x])
 		sentence = ""
 	# Else just add it to the sentence
 	else:
 		sentence += totalInformationSplit[x] + " "
 
-# At this point, sentence should be empty
+# At this point, sentence should be empty, but there could be something in it, if I figure out that we want the end
+# of the sentence that doesn't close with a terminal then I'll add it back
+#sentenceArray.append(sentence + totalInformationSplit[x])
 
 
 # Having problems with Russian abbreviated names and initials, maybe we can try looking at the last name
